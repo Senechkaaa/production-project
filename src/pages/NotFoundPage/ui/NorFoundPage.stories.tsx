711e0e9359986d3ffd920233d95159363cd329d3
Senya <arsenyi9063214@gmail.com>
2025-01-24 00:19:57 +0300

    fix linter

--- src/pages/NotFoundPage/ui/NorFoundPage.stories.tsx
+++ src/pages/NotFoundPage/ui/NorFoundPage.stories.tsx
@@ -11,8 +11,8 @@ export default {
     },
 } as ComponentMeta<typeof NotFoundPage>
 
-const Template: ComponentStory<typeof NotFoundPage> = (args) => (
-    <NotFoundPage {...args} />
+const Template: ComponentStory<typeof NotFoundPage> = () => (
+    <NotFoundPage />
 )
 
 export const Normal = Template.bind({})
